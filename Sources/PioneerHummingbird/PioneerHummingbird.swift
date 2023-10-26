015369cae831f8e7e0331e1680381d7320dbd44c
Vincent <thisoneis4business@gmail.com>
2023-10-26 17:03:28 +1300

    feat: Updated docs again

--- Sources/PioneerHummingbird/PioneerHummingbird.swift
+++ Sources/PioneerHummingbird/PioneerHummingbird.swift
@@ -1,4 +1,21 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public struct PioneerHummingbird {}
+import Hummingbird
+import Pioneer
+
+public struct PioneerHummingbird {
+    private static func ok() throws {
+        let app = HBApplication()
+        let server = Pioneer<HBApplication, HBRequest>(
+            schema: try .init(query: .init(name: "Query", fields: [:])),
+            resolver: app
+        )
+        app.router.on("/graphql", method: .POST, options: .editResponse) { req in 
+            try await server.httpHandler(
+                req: req,
+                context: { req, _ in req }
+            )
+        }
+    }
+}
